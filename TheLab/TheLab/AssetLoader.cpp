fae961e50866b9976bc5e1928cb02e7ba5e5043a
(Student  C00313555) Daemon Medeiros <C00313555@setu.ie>
2026-03-12 13:14:40 +0000

    Array for texture names in AssetLoader

--- TheLab/TheLab/AssetLoader.cpp
+++ TheLab/TheLab/AssetLoader.cpp
@@ -25,33 +25,43 @@ void AssetLoader::loadTextures()
 		{
 			case 0:
 				// textures[0] = LoadTexture("");
+				// textureNames[0] = "";
 				break;
 			case 1:
 				// textures[1] = LoadTexture("");
+				// textureNames[1] = "";
 				break;
 			case 2:
 				// textures[2] = LoadTexture("");
+				// textureNames[2] = "";
 				break;
 			case 3:
 				// textures[3] = LoadTexture("");
+				// textureNames[3] = "";
 				break;
 			case 4:
 				// textures[4] = LoadTexture("");
+				// textureNames[4] = "";
 				break;
 			case 5:
 				// textures[5] = LoadTexture("");
+				// textureNames[5] = "";
 				break;
 			case 6:
 				// textures[6] = LoadTexture("");
+				// textureNames[6] = "";
 				break;
 			case 7:
 				// textures[7] = LoadTexture("");
+				// textureNames[7] = "";
 				break;
 			case 8:
 				// textures[8] = LoadTexture("");
+				// textureNames[8] = "";
 				break;
 			case 9:
 				// textures[9] = LoadTexture("");
+				// textureNames[9] = "";
 				break;
 		}
 		loadProgress++;
--- TheLab/TheLab/AssetLoader.hpp
+++ TheLab/TheLab/AssetLoader.hpp
@@ -2,6 +2,7 @@
 #define ASSETLOADER_HPP
 
 #include <raylib.h>
+#include <string>
 
 class AssetLoader
 {
@@ -15,6 +16,8 @@ public:
 
 private:
 	static const int TOTAL_TEXTURE_COUNT{ 10 };
+
+	std::string textureNames[TOTAL_TEXTURE_COUNT];
 	Texture2D textures[TOTAL_TEXTURE_COUNT];
 	int loadProgress{ 0 };
 	bool finishedLoading{ false };
