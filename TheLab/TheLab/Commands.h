75d6461badf2b2845c2c9e4ecf1b59cab64a06cc
(Student  C00313555) Daemon Medeiros <c00313555@setu.ie>
2026-03-12 12:32:27 +0000

    Added GameStates and Command Enums

---
+++ TheLab/TheLab/Commands.h
@@ -0,0 +1,12 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+typedef enum Command {
+	CMD_NONE,
+	CMD_MOVE_UP,
+	CMD_MOVE_DOWN,
+	CMD_MOVE_LEFT,
+	CMD_MOVE_RIGHT
+} Command;
+
+#endif
---
+++ TheLab/TheLab/GameStates.h
@@ -0,0 +1,11 @@
+#ifndef GAMESTATES_H
+#define GAMESTATES_H
+
+typedef enum GameState {
+	GS_NONE,
+	GS_START,
+	GS_PLAY,
+	GS_END
+} GameState;
+
+#endif
